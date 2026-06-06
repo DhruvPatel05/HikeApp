299c9a53113dea71d9afe387f85dc2df6d40bc5c
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-06 16:27:58 -0700

    CardView UI updated.

--- HikeApp/HikeApp/Component/CardView.swift
+++ HikeApp/HikeApp/Component/CardView.swift
@@ -57,7 +57,24 @@ struct CardView: View {
                         .scaledToFit()
                 }
                 //MARK: - FOOTER
-            }
+                Button {
+                  //Action: Generate a Random Number
+                    print("The Button was pressed.")
+                } label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(
+                            LinearGradient( colors:[
+                                .customGreenLight,.customGreenMedium
+                            ],
+                                    startPoint: .top,
+                                    endPoint: .bottom)
+                        )
+                        .shadow(color: .black.opacity(0.25), radius:0.25,x:1,y:2)
+                }
+                .buttonStyle(GradientButton())
+            } // : ZStack
         }// : CARD
         .frame(width:320,height:570)
     }
