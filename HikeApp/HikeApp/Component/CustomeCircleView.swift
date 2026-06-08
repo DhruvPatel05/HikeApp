f2862c4c9346fcf196653a33061605bdecf24894
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-07 19:12:22 -0700

    Custom Animation with bubble added in main cardview.

--- HikeApp/HikeApp/Component/CustomeCircleView.swift
+++ HikeApp/HikeApp/Component/CustomeCircleView.swift
@@ -10,20 +10,23 @@ import SwiftUI
 struct CustomeCircleView: View {
     @State private var isAnimateGradient: Bool = false
     var body: some View {
-        Circle()
-            .fill(
-                LinearGradient(colors:[Color.colorIndigoMedium,Color.colorSalmonLight
-                                      ],
-                               startPoint:isAnimateGradient ? .topLeading : .bottomLeading,
-                               endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing
-                              )
-            )
-            .onAppear {
-                withAnimation(.linear(duration: 3.0).repeatForever(autoreverses: true)) {
-                    isAnimateGradient.toggle()
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(colors:[Color.colorIndigoMedium,Color.colorSalmonLight
+                                          ],
+                                   startPoint:isAnimateGradient ? .topLeading : .bottomLeading,
+                                   endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing
+                                  )
+                )
+                .onAppear {
+                    withAnimation(.linear(duration: 3.0).repeatForever(autoreverses: true)) {
+                        isAnimateGradient.toggle()
+                    }
                 }
-            }
-            .frame(width: 256,height:256)
+            MotionAnimationView()
+        }//: ZSTACK
+        .frame(width: 256,height:256)
     }
 }
 
